c3189c0f5c1ff7dea457ba8c4ce1ad83fea9cef8
shikhar <shikhar21121999@gmail.com>
2020-12-12 09:28:01 +0530

    todays work

--- reactprojectv1/src/index.js
+++ reactprojectv1/src/index.js
@@ -2,48 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// clock component using class 
-class Clock extends React.Component {
+// // clock component using class 
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.setState = {date: new Date()};
+//   }
+
+//   // life cyle mount method
+//   // This is run when component gets loaded into the UI
+
+//   componentDidMount() {
+//     this.timerId = setInterval(
+//       () => this.tick(),
+//       1000
+//     );
+//   }
+
+//   componwilldisconnect() {
+//     clearInterval(this.timerId);
+//   }
+
+//   tick() {
+//     this.setState(
+//       {
+//         date: new Date()
+//       }
+//     );
+//   }
+  
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is a clock</h1>
+//         <h2>Current time is </h2>
+//         <span>{this.state.date.toLocaleTimeString()}</span>
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <Clock />,
+//   document.getElementById('root')
+
+// )
+
+
+class Counter extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  // life cyle mount method
-  // This is run when component gets loaded into the UI
-
-  componentDidMount() {
-    this.timerId = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componwilldisconnect() {
-    clearInterval(this.timerId);
-  }
-
-  tick() {
-    this.setState(
-      {
-        date: new Date()
-      }
-    );
+    super(props)
+  
+    this.state = {
+      count : this.props.initial_val
+    }
   }
   
+  increment() {
+    this.setState({count:this.state.count+1})
+  }
+
+  increment_cnt(){
+    this.setState({count:this.state.count+1});
+  }
+
   render() {
     return (
       <div>
-        <h1>This is a clock</h1>
-        <h2>Current time is </h2>
-        <span>{this.state.date.toLocaleTimeString()}</span>
+        <div>count is - {this.state.count}</div>
+        <button onClick={() => this.increment()}>Increment</button>
       </div>
-    );
+    )
+    
   }
 }
 
 ReactDOM.render(
-  <Clock />,
-  document.getElementById('root')
-
+  <Counter initial_val={5}/>,
+  document.getElementById('root'),
 )
